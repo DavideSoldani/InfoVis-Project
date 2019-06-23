5f5e521b9c31e10c01d06c462a502d46484069c7
giorgiamarini <giorgiaj.marini@gmail.com>
2019-06-23 19:18:37 +0200

    Added dropdown menu

--- js/main.js
+++ js/main.js
@@ -1,14 +1,17 @@
 var graphs;
 d3.json("js/json/graphs.json").then(function(data) {
   graphs=data;
-  var example = graphs.simple_traingulation;
+  var example = graphs.simple_triangulation;
   //var example = graphs.nine_degree;
   // var example = graphs.eleven_degree;
   //var example = graphs.sixteen_degree;
-  var result_arcs = creaSchnyderRealizers(example);
-  find_coordinates(example, result_arcs);
+
+  drawGraph(example);
 });
 
 
-
+function drawGraph(graph){
+  var result_arcs = creaSchnyderRealizers(graph);
+  find_coordinates(graph, result_arcs);
+}
 
