1f85af5b339665a74f26154eb1e3961cf5ea6c63
giorgiamarini <giorgiaj.marini@gmail.com>
2019-06-23 20:05:59 +0200

    Modified dropdown menu

--- js/main.js
+++ js/main.js
@@ -2,11 +2,13 @@ var graphs;
 d3.json("js/json/graphs.json").then(function(data) {
   graphs=data;
   var example = graphs.simple_triangulation;
-  //var example = graphs.nine_degree;
-  // var example = graphs.eleven_degree;
-  //var example = graphs.sixteen_degree;
-
   drawGraph(example);
+  var dropdown = document.getElementById("dropdown");
+  for (var el in graphs){
+    console.log(el);
+    var tag = "<option value=" + el + ">" + el + "<//option>".toString();
+    dropdown.innerHTML+=tag;
+  }
 });
 
 
